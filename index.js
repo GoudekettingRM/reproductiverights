f8064051e7b3cfe47646697d0cf06e11b392d609
GoudekettingRM <r.goudeketting@gmail.com>
2025-01-21 14:06:22 +0100

    Update to make dropdowns work

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
+class Accordion {
+  static initialize() {
+    console.log('hi');
+    const accordions = document.querySelectorAll('.usa-accordion');
+    console.log(accordions);
+    accordions.forEach((accordion) => {
+      const acc = new Accordion(accordion);
+      acc.init();
+    });
+  }
 
-!function(){"use strict";var t,e="usa-js-loading";function n(){document.documentElement.classList.remove(e)}document.documentElement.classList.add(e),t=setTimeout(n,8e3),document.addEventListener("load",function e(){window.uswdsPresent&&(clearTimeout(t),n(),document.removeEventListener("load",e,!0))},!0)}();
-//# sourceMappingURL=uswds-init.min.js.map
+  constructor(accordion) {
+    this.accordion = accordion;
+    this.button = accordion.querySelector('.usa-accordion__button');
+    this.content = accordion.querySelector('.usa-accordion__content');
+    this.content.style.display = 'none';
+    this.contentShown = false;
+    this.button.setAttribute('aria-expanded', false);
+    this.content.hidden = true;
+  }
 
+  init() {
+    // this.accordion.addEventListener('click', () => this.handleStateChange());
+    this.button.addEventListener('click', () => this.handleStateChange());
+    this.button.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        handleStateChange();
+      }
+    });
+  }
 
+  handleStateChange() {
+    if (this.contentShown) {
+      this.content.style.display = 'none';
+      this.contentShown = false;
+      this.button.setAttribute('aria-expanded', false);
+      this.content.hidden = true;
+    } else {
+      this.content.style.display = 'block';
+      this.contentShown = true;
+      this.button.setAttribute('aria-expanded', true);
+      this.content.hidden = false;
+    }
+  }
 }
-/*
-     FILE ARCHIVED ON 01:42:40 Jan 15, 2025 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 11:49:59 Jan 21, 2025.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.586
-  exclusion.robots: 0.021
-  exclusion.robots.policy: 0.009
-  esindex: 0.011
-  cdx.remote: 19.815
-  LoadShardBlock: 136.637 (3)
-  PetaboxLoader3.datanode: 128.1 (4)
-  load_resource: 147.736
-  PetaboxLoader3.resolve: 133.041
-*/
+Accordion.initialize();
